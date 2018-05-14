790361ee08a1c8d7b19d2051658619a9ab94e340
Jared Sturdy <sturdy@cern.ch>
2018-05-14 10:26:57 +0200

    update file permissions

--- scriptlets/installrpm.sh
+++ scriptlets/installrpm.sh
@@ -15,12 +15,17 @@ find %{buildroot} -wholename '*site-packages/gempython/__init__.py' -delete
 find %{buildroot} -wholename '*site-packages/gempython/__init__.pyc' -delete
 find %{buildroot} -wholename '*site-packages/gempython/__init__.pyo' -delete
 find %{buildroot} -type f -exec chmod a+r {} \;
+find %{buildroot} -type f -iname '*.cfg' -exec chmod a-x {} \;
 
 cp INSTALLED_FILES INSTALLED_FILES.backup
 cat INSTALLED_FILES.backup|egrep -v 'gempython/__init__.py*' > INSTALLED_FILES
 # set permissions
 cat <<EOF >>INSTALLED_FILES
-%attr(-,root,root) /opt/cmsgemos/bin/*.py
+%attr(0755,root,root) /opt/cmsgemos/bin/*.py
+%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.py
+%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.pyo
+%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.pyc
+%attr(0755,root,root) /usr/lib/python*/site-packages/gempython/scripts/*.py
 %exclude /usr/lib/python*/site-packages/gempython/__init__.py
 %exclude /usr/lib/python*/site-packages/gempython/__init__.pyc
 %exclude /usr/lib/python*/site-packages/gempython/__init__.pyo
