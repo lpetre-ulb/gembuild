3dca5aebf3639c2cca423c7323c6d558265509dd
Jared Sturdy <sturdy@cern.ch>
2018-05-14 14:34:21 +0200

    fix unnecessary check which broke slc6 build

--- scriptlets/installrpm.sh
+++ scriptlets/installrpm.sh
@@ -22,13 +22,7 @@ cat INSTALLED_FILES.backup|egrep -v 'gempython/__init__.py*' > INSTALLED_FILES
 # set permissions
 cat <<EOF >>INSTALLED_FILES
 %attr(0755,root,root) /opt/cmsgemos/bin/*.py
-%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.py
-%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.pyo
-%attr(0644,root,root) /usr/lib/python*/site-packages/gempython/**/*.pyc
 %attr(0755,root,root) /usr/lib/python*/site-packages/gempython/scripts/*.py
-%exclude /usr/lib/python*/site-packages/gempython/__init__.py
-%exclude /usr/lib/python*/site-packages/gempython/__init__.pyc
-%exclude /usr/lib/python*/site-packages/gempython/__init__.pyo
 EOF
 echo "Modified INSTALLED_FILES"
 cat INSTALLED_FILES
