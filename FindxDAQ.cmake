ab3a9baefca53ba378f40bf06acd108f6e5f3e7e
Louis Moureaux <lmoureau@ulb.ac.be>
2019-02-27 00:06:22 +0100

    FindxDAQ: Don't pollute global scope
    And only include threads in really needed.

--- FindxDAQ.cmake
+++ FindxDAQ.cmake
@@ -6,19 +6,13 @@
 
 include(CMakeParseArguments)
 
-# Check for threading libraries only if required
-set(xdaq_need_threads FALSE)
-
 # List of all supported libs
 set(xdaq_all_libs "")
 
 # Declares a library that can be required using the COMPONENTS argument
-function(xdaq_library name)
+function(_xdaq_library name)
     cmake_parse_arguments(ARG "THREADS" "HEADER" "DEPENDS" "" ${ARGN})
 
-    if(${ARG_THREADS})
-        set(xdaq_need_threads TRUE PARENT_SCOPE)
-    endif()
     set(xdaq_${name}_threads ${ARG_THREADS} PARENT_SCOPE)
     set(xdaq_${name}_header ${ARG_HEADER} PARENT_SCOPE)
     set(xdaq_${name}_depends ${ARG_DEPENDS} PARENT_SCOPE)
@@ -27,35 +21,36 @@ function(xdaq_library name)
 endfunction()
 
 # List all supported libs and their dependencies
-xdaq_library(asyncresolv HEADER "asyncresolv/config.h")
-xdaq_library(cgicc HEADER "cgicc/Cgicc.h")
-xdaq_library(config HEADER "config/PackageInfo.h" DEPENDS xcept)
-xdaq_library(i2o HEADER "i2o/version.h" DEPENDS config toolbox xcept)
-xdaq_library(log4cplus HEADER "log4cplus/config.hxx")
-xdaq_library(logudpappender HEADER "log4cplus/log4judpappender.h"
-                            DEPENDS config log4cplus)
-xdaq_library(logxmlappender HEADER "log/xmlappender/version.h"
-                            DEPENDS config log4cplus)
-xdaq_library(mimetic HEADER "mimetic/version.h")
-xdaq_library(peer HEADER "pt/version.h" DEPENDS config toolbox xcept xoap THREADS)
-xdaq_library(toolbox HEADER "toolbox/version.h"
-                     DEPENDS asyncresolv cgicc log4cplus THREADS)
-xdaq_library(tstoreclient HEADER "tstore/client/version.h")
-xdaq_library(tstoreutils HEADER "tstore/utils/version.h")
-xdaq_library(tstore HEADER "tstore/version.h" DEPENDS tstoreclient tstoreutils xalan-c)
-xdaq_library(xalan-c HEADER "xalanc/Include/XalanVersion.hpp")
-xdaq_library(xcept HEADER "xcept/version.h" DEPENDS config toolbox)
-xdaq_library(xdata HEADER "xdata/version.h"
-                   DEPENDS config mimetic toolbox xcept xerces-c xoap THREADS)
-xdaq_library(xdaq HEADER "xdaq/version.h"
-                  DEPENDS config log4cplus logudpappender logxmlappender peer
-                          toolbox xcept xdata xerces-c xgi xoap)
-xdaq_library(xdaq2rc HEADER "xdaq2rc/version.h"
-                     DEPENDS config log4cplus toolbox xdaq xdata xerces-c xoap THREADS)
-xdaq_library(xerces-c HEADER "xercesc/util/XercesVersion.hpp")
-xdaq_library(xgi HEADER "xgi/version.h"
-                 DEPENDS cgicc config toolbox xcept xerces-c THREADS)
-xdaq_library(xoap HEADER "xoap/version.h" DEPENDS config toolbox xcept xerces-c)
+_xdaq_library(asyncresolv HEADER "asyncresolv/config.h")
+_xdaq_library(cgicc HEADER "cgicc/Cgicc.h")
+_xdaq_library(config HEADER "config/PackageInfo.h" DEPENDS xcept)
+_xdaq_library(i2o HEADER "i2o/version.h" DEPENDS config toolbox xcept)
+_xdaq_library(log4cplus HEADER "log4cplus/config.hxx")
+_xdaq_library(logudpappender HEADER "log4cplus/log4judpappender.h"
+                             DEPENDS config log4cplus)
+_xdaq_library(logxmlappender HEADER "log/xmlappender/version.h"
+                             DEPENDS config log4cplus)
+_xdaq_library(mimetic HEADER "mimetic/version.h")
+_xdaq_library(peer HEADER "pt/version.h" DEPENDS config toolbox xcept xoap THREADS)
+_xdaq_library(toolbox HEADER "toolbox/version.h"
+                      DEPENDS asyncresolv cgicc log4cplus THREADS)
+_xdaq_library(tstoreclient HEADER "tstore/client/version.h")
+_xdaq_library(tstoreutils HEADER "tstore/utils/version.h")
+_xdaq_library(tstore HEADER "tstore/version.h"
+                     DEPENDS tstoreclient tstoreutils xalan-c)
+_xdaq_library(xalan-c HEADER "xalanc/Include/XalanVersion.hpp")
+_xdaq_library(xcept HEADER "xcept/version.h" DEPENDS config toolbox)
+_xdaq_library(xdata HEADER "xdata/version.h"
+                    DEPENDS config mimetic toolbox xcept xerces-c xoap THREADS)
+_xdaq_library(xdaq HEADER "xdaq/version.h"
+                   DEPENDS config log4cplus logudpappender logxmlappender peer
+                           toolbox xcept xdata xerces-c xgi xoap)
+_xdaq_library(xdaq2rc HEADER "xdaq2rc/version.h"
+                      DEPENDS config log4cplus toolbox xdaq xdata xerces-c xoap THREADS)
+_xdaq_library(xerces-c HEADER "xercesc/util/XercesVersion.hpp")
+_xdaq_library(xgi HEADER "xgi/version.h"
+                  DEPENDS cgicc config toolbox xcept xerces-c THREADS)
+_xdaq_library(xoap HEADER "xoap/version.h" DEPENDS config toolbox xcept xerces-c)
 
 # If the list of libs isn't specified, assume all of them are needed
 if(NOT xDAQ_FIND_COMPONENTS)
@@ -67,21 +62,28 @@ if(xDAQ_FIND_VERSION)
     message(WARNING "xDAQ version ${xDAQ_FIND_VERSION} was requested, but version checking is not supported")
 endif()
 
+# Check for threading libraries only if required
+set(xdaq_need_threads FALSE)
+
+# Add dependencies to the list of libs
+set(xdaq_required_libs "")
+foreach(lib ${xDAQ_FIND_COMPONENTS})
+    list(APPEND xdaq_required_libs ${lib})
+    list(APPEND xdaq_required_libs ${xdaq_${lib}_depends})
+
+    if(xdaq_${name}_threads)
+        set(xdaq_need_threads TRUE)
+    endif()
+endforeach()
+list(REMOVE_DUPLICATES xdaq_required_libs)
+
 # Threads
 if(xdaq_need_threads)
     find_package(Threads QUIET)
 endif()
 
-# Add dependencies to the list of libs
-set(list_of_libs "")
-foreach(lib ${xDAQ_FIND_COMPONENTS})
-    list(APPEND list_of_libs ${lib})
-    list(APPEND list_of_libs ${xdaq_${lib}_depends})
-endforeach()
-list(REMOVE_DUPLICATES list_of_libs)
-
 # Creates an imported target for the given lib
-function(xdaq_import_lib name)
+function(_xdaq_import_lib name)
     # Check that the lib exists
     list(FIND "${xdaq_all_libs}" ${name} found)
     if(NOT found EQUAL -1)
@@ -103,6 +105,8 @@ function(xdaq_import_lib name)
         PATH_SUFFIXES lib lib64
         DOC "Root directory of the xDAQ installation")
 
+    mark_as_advanced(xdaq_${name}_library)
+
     if(NOT xdaq_${name}_library)
         if(xDAQ_FIND_REQUIRED)
             message(SEND_ERROR "Could not find shared object file for xDAQ library ${name}")
@@ -121,6 +125,8 @@ function(xdaq_import_lib name)
         PATHS /opt/xdaq/
         PATH_SUFFIXES include
         DOC "Root directory of the xDAQ installation")
+
+    mark_as_advanced(xdaq_${name}_header_location)
 
     if(NOT xdaq_${name}_header_location)
         if(xDAQ_FIND_REQUIRED)
@@ -180,28 +186,31 @@ function(xdaq_import_lib name)
 endfunction()
 
 # Import all libs
-foreach(lib IN LISTS list_of_libs)
-    xdaq_import_lib(${lib})
+foreach(lib IN LISTS xdaq_required_libs)
+    _xdaq_import_lib(${lib})
 endforeach()
 
 # Print some debug info
 message(STATUS "Found the following xDAQ libraries:")
-foreach(lib ${list_of_libs})
+foreach(lib ${xdaq_required_libs})
     message(STATUS "  ${lib}")
 endforeach()
 
 # i2o requires an additional definition
-list(FIND list_of_libs i2o found)
+list(FIND xdaq_required_libs i2o found)
 if(NOT found EQUAL -1)
     set_property(TARGET xDAQ::i2o
                  APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS
                  LITTLE_ENDIAN__)
 endif()
+unset(found)
 
 # toolbox requires libuuid from the system
-list(FIND list_of_libs toolbox found)
+list(FIND xdaq_required_libs toolbox found)
 if(NOT found EQUAL -1)
     find_library(xdaq_uuid_library uuid)
+    mark_as_advanced(xdaq_uuid_library)
+
     # Maybe we shouldn't fail if not REQUIRED...
     if(NOT xdaq_uuid_library)
         message(SEND_ERROR "Could not find libuuid, required by xDAQ library toolbox")
@@ -210,3 +219,15 @@ if(NOT found EQUAL -1)
                  APPEND PROPERTY INTERFACE_LINK_LIBRARIES
                  ${xdaq_uuid_library})
 endif()
+unset(found)
+
+# Cleanup
+foreach(lib ${xdaq_all_libs})
+    unset(xdaq_${name}_threads)
+    unset(xdaq_${name}_header)
+    unset(xdaq_${name}_depends)
+endforeach()
+
+unset(xdaq_need_threads)
+unset(xdaq_all_libs)
+unset(xdaq_required_libs)
